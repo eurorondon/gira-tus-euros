dbf60149e153ed943c1e903429de8ce778abefbb
eurorondon03 <eurorondon03@gmail.com>
2024-03-03 03:56:30 -0400

    modificando stickey  scroll position

--- src/app/components/Whatsapp.jsx
+++ src/app/components/Whatsapp.jsx
@@ -1,21 +1,42 @@
+"use client";
 import React from "react";
 
 function Whatsapp() {
-  return (
-    <div
-      className=" rounded-t-2xl sticky bottom-0 w-full"
-      style={{ backgroundColor: "#DCEC20" }}
-    >
-      <div className="p-4 " style={{ color: "#545353" }}>
-        <h3 className=" text-md font-extrabold text-center">
-          CONTINUA TU CAMBIO POR WHATSAPP <br />
-          <span className="font-semibold text-sm">
-            O Pide acceso a nuestro grupo para referencias
-          </span>
-        </h3>
+  const [showObject, setShowObject] = React.useState(false);
+  React.useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.pageYOffset;
+      const triggerHeight = 10; // Altura de desencadenamiento en píxeles
+
+      if (scrollPosition > triggerHeight) {
+        setShowObject(true);
+      } else {
+        setShowObject(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  if (showObject)
+    return (
+      <div
+        className=" rounded-t-2xl sticky bottom-0 w-full"
+        style={{ backgroundColor: "#DCEC20" }}
+      >
+        <div className="p-4 " style={{ color: "#545353" }}>
+          <h3 className=" text-md font-extrabold text-center">
+            CONTINUA TU CAMBIO POR WHATSAPP <br />
+            <span className="font-semibold text-sm">
+              O Pide acceso a nuestro grupo para referencias
+            </span>
+          </h3>
+        </div>
       </div>
-    </div>
-  );
+    );
 }
 
 export default Whatsapp;
